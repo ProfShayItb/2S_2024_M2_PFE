6bd14e1f8d6f8ac81bbf4271f2a3a5e4ed50e9b4
ProfShayItb <sharlayne.fonseca@docente.fieb.edu.br>
2024-08-07 13:01:43 -0300

    first commit

---
+++ 0208/0208.js
@@ -0,0 +1,5 @@
+var n1 = Number(prompt('Digite um número: '))
+
+var n2 = Number(prompt('Digite um número: '))
+
+alert(n1+n2)
---
+++ index.js
@@ -0,0 +1,23 @@
+// //Comentário de uma linha
+// /* Comentário de mais que uma linha */
+
+// /*Recursos Nativos*/
+// console.log('Boa noite!')//imprimi texto no console
+// console.info('Informação')
+// console.warn('Cuidado!')
+// console.error('Mensagens de erro')
+
+// //Caixas de diálogo:
+// // alert('Janela de alerta!')//Janela de alerta
+// // confirm('Janela de confirmação')//janela de confirmação
+// // prompt('Qual seu nome?')//Entrada de usuário
+
+// //Varíaveis
+// //var, let, const
+
+// var nomeDoUsuario = prompt('Qual seu nome?')
+// alert('Bem vindo(a) ' + nomeDoUsuario + '!')
+
+
+
+
